2e99fe48c2a7d57d375b86e9461c9a2e25cdf09c
Cris1037 <128873608+Cris1037@users.noreply.github.com>
2025-01-27 20:31:59 -0700

    root page update, week 2 update
    Root page updated to allow access to week 2
    week 2 page updated to access to root page

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,14 +2,18 @@ const StudentInfo = () => {
     return (
       <div className="p-4 bg-gray-800 text-white rounded-lg">
         <h1 className="text-xl font-bold mb-2">Cristian</h1>
-        <a
+        <p><a
           href="https://github.com/Cris1037"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-400 hover:underline"
         >
-          My GitHub Repository
-        </a>
+            My GitHub Repository
+        </a></p>
+        <p>
+            <a href="../">Back to Home</a>
+        </p>
+
       </div>
     );
   };
