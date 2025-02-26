48c0162829f73262a4798dfc188a2a8667a1c099
Cris1037 <128873608+Cris1037@users.noreply.github.com>
2025-02-25 20:49:10 -0700

    Update new-item.js

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -32,7 +32,7 @@ export default function NewItem(){
 
         console.log(item);
 
-        alert(`Name: ${name}, Quantity: ${quantity}, Category: new`);
+        alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
         setName("");
         setQuantity(1);
     }
@@ -44,33 +44,33 @@ export default function NewItem(){
 
                 <p className="text-2xl">Quantity: {quantity}</p>
                 <button 
-                    className={`bg-blue-500 text-white py-2 px-4 rounded w-12 ${quantity === 1 ? 'bg-gray-500 cursor-not-allowed' : 'hover:bg-blue-700'} p-3`} 
+                    className={`bg-blue-500 text-white py-2 rounded w-12 ${quantity === 1 ? 'bg-gray-500 cursor-not-allowed' : 'hover:bg-blue-700'} `} 
                     onClick={decrement} 
                     disabled={quantity === 1}
                 >
                     -
                 </button>
                 <button 
-                    className={`bg-blue-500 text-white py-2 px-4 rounded w-12 ${quantity === 20 ? 'bg-gray-500 cursor-not-allowed' : 'hover:bg-blue-700'} ml-1`} 
+                    className={`bg-blue-500 text-white py-2 rounded w-12 ${quantity === 20 ? 'bg-gray-500 cursor-not-allowed' : 'hover:bg-blue-700'} m-1`} 
                     onClick={increment} 
                     disabled={quantity === 20}
                 >
                     +
                 </button>
-                <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                    <option value="produce">Produce</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="bakery">Bakery</option>
-                    <option value="meat">Meat</option>
-                    <option value="frozen foods">Frozen Foods</option>
-                    <option value="canned goods">Canned Goods</option>
-                    <option value="dry goods">Dry Goods</option>
-                    <option value="beverages">Beverages</option>
-                    <option value="snacks">Snacks</option>
-                    <option value="household">Household</option>
-                    <option value="other">Other</option>
+                <select className="m-1" value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Bakery">Bakery</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Frozen Foods">Frozen Foods</option>
+                    <option value="Canned Goods">Canned Goods</option>
+                    <option value="Dry Goods">Dry Goods</option>
+                    <option value="Beverages">Beverages</option>
+                    <option value="Snacks">Snacks</option>
+                    <option value="Household">Household</option>
+                    <option value="Other">Other</option>
                 </select>
-                <button className="bg-blue-500 text-white p-2 rounded" onClick={handleSubmit}>Submit</button>
+                <button className="bg-blue-500 text-white p-2 rounded m-1" onClick={handleSubmit}>Submit</button>
             </div>
         </div>
     );
